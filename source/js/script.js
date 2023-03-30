5ea1e3e2d133eaa7050a0002701a134ebf04cae5
Mikhail <65595208+mikhail7mz@users.noreply.github.com>
2023-03-30 08:28:37 +0300

    make header

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,13 @@
 // Menu for the mobile version
 
-const navigationToggle = document.querySelector('.navigation__toggle');
-const navigationBody = document.querySelector('.navigation__body');
+const navigationToggle = document.querySelector('.header__navigation-toggle');
+const navigationBody = document.querySelector('.header__navigation-list');
 
-document.querySelector(".navigation--no-js").classList.remove("navigation--no-js");
+document.querySelector(".header__navigation--no-js").classList.remove("header__navigation--no-js");
 
 navigationToggle.addEventListener('click', (e) => {
   e.preventDefault();
 
-  navigationToggle.classList.toggle('navigation__toggle--close');
-  navigationBody.classList.toggle('navigation__body--opened');
+  navigationToggle.classList.toggle('header__navigation-toggle--close');
+  navigationBody.classList.toggle('header__navigation-list--opened');
 });
